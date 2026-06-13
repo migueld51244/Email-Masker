c895b836aebbe012fadc242f4a8dcd8db5140dbe
MiguelD <migueldoliveira142@gmail.com>
2026-06-13 17:17:26 +0100

    JS code now masks the email on the input field

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let email = document.getElementById("email-area");
+
 function maskEmail(email) {
   let atIndex = email.indexOf('@');
   let firstChar = email[0];
@@ -7,9 +9,13 @@ function maskEmail(email) {
   // Calculate how many characters need to be replaced with '*'
   let starsToRepeat = atIndex - 2;
   let maskedPart = '*'.repeat(starsToRepeat);
-  
+  console.log(firstChar + maskedPart + lastChar + domain);
   return firstChar + maskedPart + lastChar + domain;
 }
 
-const email = 'miguel34474@esfundao.pt';
-console.log(maskEmail(email));
+document.getElementById("submit").addEventListener(
+  "click", () => {
+    let value = email.value;
+    maskEmail(value);
+  }
+);
